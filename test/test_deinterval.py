37ec36ce60f8f8a5069dbcdab6d772608619aeab
John T. Wodder II <git@varonathe.org>
2017-10-06 14:34:26 +0000

    Basic tests of `deinterval()`

---
+++ test/test_deinterval.py
@@ -0,0 +1,30 @@
+import pytest
+from   derange  import deinterval
+
+def delta1(a,b):
+    return abs(a-b) <= 1
+
+@pytest.mark.parametrize('predicate,iterable,output', [
+    (delta1, [], []),
+    (delta1, [42], [(42, 42)]),
+    (delta1, [2016, 2015, 2016], [(2015, 2016)]),
+    (delta1, [1, 5, 2, 4, 3], [(1,5)]),
+    (delta1, [1, 1, 5, 5, 2, 2, 4, 4, 3, 3], [(1,5)]),
+    (delta1, [1, 5, 3], [(1,1), (3,3), (5,5)]),
+    (
+        delta1,
+        [3,4,5,6,7,10,11,12,15,16,17,19,20,21,22,23,24,42,43,44,45,46],
+        [(3,7), (10,12), (15,17), (19,24), (42,46)],
+    ),
+    (
+        delta1,
+        [46,45,44,43,42,24,23,22,21,20,19,17,16,15,12,11,10,7,6,5,4,3],
+        [(3,7), (10,12), (15,17), (19,24), (42,46)],
+    ),
+    (delta1, [1, 1.5, 2.4], [(1, 2.4)]),
+    (delta1, [1, 1.5, 2.6], [(1, 1.5), (2.6, 2.6)]),
+    (delta1, [1, 1.5, 1.4], [(1, 1.5)]),
+    (delta1, [1, 1.5, 2.6, 2], [(1, 2.6)]),
+])
+def test_deinterval(predicate, iterable, output):
+    assert deinterval(predicate, iterable) == output
